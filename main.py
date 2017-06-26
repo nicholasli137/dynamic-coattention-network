57514b1f984208c5a83b987ef2ec7b538cf0d805
Nicholas Li <nicholas.li.137@gmail.com>
2017-06-25 22:48:48 -0400

    feat: make compatible with tf 1.0, cleanup code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 """
 Tensorflow implementation of https://arxiv.org/abs/1611.01604.
 """
@@ -6,6 +5,7 @@ import time
 
 import numpy as np
 import tensorflow as tf
+
 tf.logging.set_verbosity(tf.logging.INFO)
 
 import batch_reader
@@ -49,162 +49,165 @@ tf.app.flags.DEFINE_integer('random_seed', 123, 'A seed value for randomness.')
 
 
 def _runningAvgLoss(loss, running_avg_loss, summary_writer, step, decay=0.999):
-  """Calculate the running average of losses."""
-  if running_avg_loss == 0:
-    running_avg_loss = loss
-  else:
-    running_avg_loss = running_avg_loss * decay + (1 - decay) * loss
-  #running_avg_loss = min(running_avg_loss, 12)
-  loss_sum = tf.Summary()
-  loss_sum.value.add(tag='running_avg_loss', simple_value=running_avg_loss)
-  summary_writer.add_summary(loss_sum, step)
-  return running_avg_loss
-
-def _train(model, data_batcher):
-  """Runs model training."""
-  with tf.device('/cpu:0'):
-    model.build_graph()
-    saver = tf.train.Saver()
-    # Train dir is different from log_root to avoid summary directory
-    # conflict with Supervisor.
-    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir)
-    sv = tf.train.Supervisor(logdir=FLAGS.log_root,
-                             is_chief=True,
-                             saver=saver,
-                             summary_op=None,
-                             save_summaries_secs=60,
-                             save_model_secs=FLAGS.checkpoint_secs,
-                             global_step=model._global_step)
-    sess = sv.prepare_or_wait_for_session(config=tf.ConfigProto(
-        allow_soft_placement=True))
-    running_avg_loss = 0
-    step = 0
-    while not sv.should_stop() and step < FLAGS.max_run_steps:
-
-      (batch_context, batch_question, batch_answer,
-       _, _, _) = data_batcher.next()
-
-      initial_guess = np.zeros((2, model._params.batch_size))
-
-      start = time.time()
-      (_, summaries, loss, train_step, s, e) = model.train(
-          sess, batch_context, batch_question, batch_answer, initial_guess)
-
-      tf.logging.info('took: %.4f sec', time.time()-start)
-      tf.logging.info('global_step: %d', train_step)
-      tf.logging.info('loss: %f', loss)
-      tf.logging.info('running_avg_loss: %f', running_avg_loss)
-
-      pred_answers = np.hstack((s, e))
-      tf.logging.info('pred_answers: {}'.format(pred_answers))
-      tf.logging.info('true_answers: {}\n'.format(batch_answer))
-
-      summary_writer.add_summary(summaries, train_step)
-      running_avg_loss = _runningAvgLoss(
-          running_avg_loss, loss, summary_writer, train_step)
-
-      step += 1
-      if step % 100 == 0:
-        summary_writer.flush()
-
-    sv.Stop()
+    """Calculate the running average of losses."""
+    if running_avg_loss == 0:
+        running_avg_loss = loss
+    else:
+        running_avg_loss = running_avg_loss * decay + (1 - decay) * loss
+    # running_avg_loss = min(running_avg_loss, 12)
+    loss_sum = tf.Summary()
+    loss_sum.value.add(tag='running_avg_loss', simple_value=running_avg_loss)
+    summary_writer.add_summary(loss_sum, step)
     return running_avg_loss
 
+
+def _train(model, data_batcher):
+    """Runs model training."""
+    with tf.device('/cpu:0'):
+        model.build_graph()
+        saver = tf.train.Saver()
+        # Train dir is different from log_root to avoid summary directory
+        # conflict with Supervisor.
+        summary_writer = tf.summary.FileWriter(FLAGS.train_dir)
+        sv = tf.train.Supervisor(logdir=FLAGS.log_root,
+                                 is_chief=True,
+                                 saver=saver,
+                                 summary_op=None,
+                                 save_summaries_secs=60,
+                                 save_model_secs=FLAGS.checkpoint_secs,
+                                 global_step=model._global_step)
+        sess = sv.prepare_or_wait_for_session(config=tf.ConfigProto(
+            allow_soft_placement=True))
+        running_avg_loss = 0
+        step = 0
+        while not sv.should_stop() and step < FLAGS.max_run_steps:
+
+            (batch_context, batch_question, batch_answer,
+             _, _, _) = data_batcher.next()
+
+            initial_guess = np.zeros((2, model._params.batch_size))
+
+            start = time.time()
+            (_, summaries, loss, train_step, s, e) = model.train(
+                sess, batch_context, batch_question, batch_answer, initial_guess)
+
+            tf.logging.info('took: %.4f sec', time.time() - start)
+            tf.logging.info('global_step: %d', train_step)
+            tf.logging.info('loss: %f', loss)
+            tf.logging.info('running_avg_loss: %f', running_avg_loss)
+
+            pred_answers = np.hstack((s, e))
+            tf.logging.info('pred_answers: {}'.format(pred_answers))
+            tf.logging.info('true_answers: {}\n'.format(batch_answer))
+
+            summary_writer.add_summary(summaries, train_step)
+            running_avg_loss = _runningAvgLoss(
+                running_avg_loss, loss, summary_writer, train_step)
+
+            step += 1
+            if step % 100 == 0:
+                summary_writer.flush()
+
+        sv.Stop()
+        return running_avg_loss
+
+
 def _eval(model, data_batcher):
-  """Runs model eval."""
-  model.build_graph()
-  saver = tf.train.Saver()
-  summary_writer = tf.train.SummaryWriter(FLAGS.eval_dir)
-  sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-  running_avg_loss = 0
-  step = 0
-  while True:
-    time.sleep(FLAGS.eval_interval_secs)
-    try:
-      ckpt_state = tf.train.get_checkpoint_state(FLAGS.log_root)
-    except tf.errors.OutOfRangeError as e:
-      tf.logging.error('Cannot restore checkpoint: %s', e)
-      continue
+    """Runs model eval."""
+    model.build_graph()
+    saver = tf.train.Saver()
+    summary_writer = tf.summary.FileWriter(FLAGS.eval_dir)
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+    running_avg_loss = 0
+    step = 0
+    while True:
+        time.sleep(FLAGS.eval_interval_secs)
+        try:
+            ckpt_state = tf.train.get_checkpoint_state(FLAGS.log_root)
+        except tf.errors.OutOfRangeError as e:
+            tf.logging.error('Cannot restore checkpoint: %s', e)
+            continue
 
-    if not (ckpt_state and ckpt_state.model_checkpoint_path):
-      tf.logging.info('No model to eval yet at %s', FLAGS.train_dir)
-      continue
+        if not (ckpt_state and ckpt_state.model_checkpoint_path):
+            tf.logging.info('No model to eval yet at %s', FLAGS.train_dir)
+            continue
 
-    tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
-    saver.restore(sess, ckpt_state.model_checkpoint_path)
+        tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
+        saver.restore(sess, ckpt_state.model_checkpoint_path)
 
-    (batch_context, batch_question, batch_answer,
-     origin_context, origin_question, origin_answer) = data_batcher.next()
+        (batch_context, batch_question, batch_answer,
+         origin_context, origin_question, origin_answer) = data_batcher.next()
 
-    initial_guess = np.zeros((2, model._params.batch_size))
+        initial_guess = np.zeros((2, model._params.batch_size))
 
-    start = time.time()
-    (summaries, loss, train_step) = model.eval(
-        sess, batch_context, batch_question, batch_answer, initial_guess)
+        start = time.time()
+        (summaries, loss, train_step) = model.eval(
+            sess, batch_context, batch_question, batch_answer, initial_guess)
 
-    tf.logging.info('took: %.4f sec', time.time()-start)
-    tf.logging.info('context:  %s', origin_context)
-    tf.logging.info('question: %s', origin_question)
-    tf.logging.info('answer: %s', origin_answer)
+        tf.logging.info('took: %.4f sec', time.time() - start)
+        tf.logging.info('context:  %s', origin_context)
+        tf.logging.info('question: %s', origin_question)
+        tf.logging.info('answer: %s', origin_answer)
 
-    summary_writer.add_summary(summaries, train_step)
-    running_avg_loss = _runningAvgLoss(
-        running_avg_loss, loss, summary_writer, train_step)
+        summary_writer.add_summary(summaries, train_step)
+        running_avg_loss = _runningAvgLoss(
+            running_avg_loss, loss, summary_writer, train_step)
 
-    tf.logging.info('global_step: %d' % train_step)
-    tf.logging.info('loss: %f' % loss)
-    tf.logging.info('running_avg_loss: %f\n' % running_avg_loss)
+        tf.logging.info('global_step: %d' % train_step)
+        tf.logging.info('loss: %f' % loss)
+        tf.logging.info('running_avg_loss: %f\n' % running_avg_loss)
 
-    if step % 100 == 0:
-      summary_writer.flush()
+        if step % 100 == 0:
+            summary_writer.flush()
+
 
 def main(unused_argv):
-  vocab = dataset.Vocab(FLAGS.vocab_path, 200000)
-  # Check for presence of required special tokens.
-  assert vocab.tokenToId(dataset.PAD_TOKEN) > 0
-  assert vocab.tokenToId(dataset.UNKNOWN_TOKEN) > 0
-  assert vocab.tokenToId(dataset.SENTENCE_START) > 0
-  assert vocab.tokenToId(dataset.SENTENCE_END) > 0
-  assert vocab.tokenToId(dataset.WORD_BEGIN) > 0
-  assert vocab.tokenToId(dataset.WORD_CONTINUE) > 0
-  assert vocab.tokenToId(dataset.WORD_END) > 0
+    vocab = dataset.Vocab(FLAGS.vocab_path, 200000)
+    # Check for presence of required special tokens.
+    assert vocab.tokenToId(dataset.PAD_TOKEN) > 0
+    assert vocab.tokenToId(dataset.UNKNOWN_TOKEN) > 0
+    assert vocab.tokenToId(dataset.SENTENCE_START) > 0
+    assert vocab.tokenToId(dataset.SENTENCE_END) > 0
+    assert vocab.tokenToId(dataset.WORD_BEGIN) > 0
+    assert vocab.tokenToId(dataset.WORD_CONTINUE) > 0
+    assert vocab.tokenToId(dataset.WORD_END) > 0
 
-  params = selector.parameters(
-      mode=FLAGS.mode,  # train, eval, decode
-      min_lr=0.01,  # min learning rate.
-      lr=0.1,  # learning rate
-      batch_size=1,
-      c_timesteps=600, # context length
-      q_timesteps=30, # question length
-      min_input_len=2,  # discard context, question < than this words
-      hidden_size=200,  # for rnn cell and embedding
-      emb_size=200,  # If 0, don't use embedding
-      max_decode_steps=4,
-      maxout_size=32,
-      max_grad_norm=2)
+    params = selector.parameters(
+        mode=FLAGS.mode,  # train, eval, decode
+        min_lr=0.01,  # min learning rate.
+        lr=0.1,  # learning rate
+        batch_size=1,
+        c_timesteps=600,  # context length
+        q_timesteps=30,  # question length
+        min_input_len=2,  # discard context, question < than this words
+        hidden_size=200,  # for rnn cell and embedding
+        emb_size=200,  # If 0, don't use embedding
+        max_decode_steps=4,
+        maxout_size=32,
+        max_grad_norm=2)
 
-  batcher = batch_reader.Generator(
-      FLAGS.data_path, vocab, params,
-      FLAGS.context_key, FLAGS.question_key, FLAGS.answer_key,
-      FLAGS.max_context_sentences, FLAGS.max_question_sentences,
-      bucketing=FLAGS.use_bucketing, truncate_input=FLAGS.truncate_input)
+    batcher = batch_reader.Generator(
+        FLAGS.data_path, vocab, params,
+        FLAGS.context_key, FLAGS.question_key, FLAGS.answer_key,
+        FLAGS.max_context_sentences, FLAGS.max_question_sentences,
+        bucketing=FLAGS.use_bucketing, truncate_input=FLAGS.truncate_input)
 
-  tf.set_random_seed(FLAGS.random_seed)
+    tf.set_random_seed(FLAGS.random_seed)
 
-  if params.mode == 'train':
-    model = selector.Model(
-        params, len(vocab), num_cpus=FLAGS.num_cpus, num_gpus=FLAGS.num_gpus)
-    _train(model, batcher)
-  elif params.mode == 'eval':
-    model = selector.Model(
-        params, len(vocab), num_cpus=FLAGS.num_cpus, num_gpus=FLAGS.num_gpus)
-    _eval(model, batcher)
-  elif params.mode == 'decode':
-    model = selector.Model(
-        params, len(vocab), num_cpus=FLAGS.num_cpus, num_gpus=FLAGS.num_gpus)
-    machine = decoder.Decoder(model, batcher, params, vocab)
-    machine.loop()
+    if params.mode == 'train':
+        model = selector.Model(
+            params, len(vocab), num_cpus=FLAGS.num_cpus, num_gpus=FLAGS.num_gpus)
+        _train(model, batcher)
+    elif params.mode == 'eval':
+        model = selector.Model(
+            params, len(vocab), num_cpus=FLAGS.num_cpus, num_gpus=FLAGS.num_gpus)
+        _eval(model, batcher)
+    elif params.mode == 'decode':
+        model = selector.Model(
+            params, len(vocab), num_cpus=FLAGS.num_cpus, num_gpus=FLAGS.num_gpus)
+        machine = decoder.Decoder(model, batcher, params, vocab)
+        machine.loop()
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
